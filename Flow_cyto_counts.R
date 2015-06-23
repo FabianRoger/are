3c0212a773d9d05345bed59a58cea2a2db913606
Fabian Roger <Fabian_Roger@gmx.de>
2015-06-23 14:34:12 +0200

    script to import, quality controle and clean the flow cytometer counts and calculate maximum biomass and stability. Set-up

---
+++ Flow_cyto_counts.R
@@ -0,0 +1,29 @@
+
+
+# FLOW CYTOMETER COUNTS i.e. CELL NUMBERS #
+
+# this script takes: 
+    # --> the flow cytometer raw data 
+    # --> the time stemp files
+    # --> the sample metadata
+
+# this script does:
+    # --> quality control 
+    # --> bias correction
+    # --> calculates maximum biomass per sample
+    # --> calculates temporal stability
+
+# this script exports:
+    # --> maximum_cell_count.txt; table with maximum cell counts
+    # --> stability.txt; a table with temporal stability of the cell counts
+
+# load packages
+################################################################################
+
+################################################################################
+
+# import files
+################################################################################
+
+################################################################################
+
